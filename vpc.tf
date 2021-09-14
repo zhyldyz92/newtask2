3da4c6ecfd82bc121d077b68eb92b263d8bf6ac5
zhyldyz92 <78001953+zhyldyz92@users.noreply.github.com>
2021-09-14 15:43:50 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,20 @@
-
+resource "google_compute_network" "test" {
+  name                    = var.gke_config["network"]
+  project                 = var.gke_config["project"]
+  auto_create_subnetworks = var.gke_config["auto_create_subnetworks"]
+}
+resource "google_compute_subnetwork" "testgkesubnet" {
+  name          = var.gke_config["subnetwork"]
+  project       = var.gke_config["project"]
+  ip_cidr_range = "10.2.0.0/27"
+  region        = var.gke_config["region"]
+  network       = google_compute_network.test.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.64.0/22"
+  }
+}
